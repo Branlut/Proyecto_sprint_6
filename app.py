66775f7d468810209e3daea36a728332abfe60ad
Branlut <brayantrigoaraya@gmail.com>
2024-07-01 11:25:19 -0400

    primer commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+car_data = pd.read_csv("C:/Users/braya/Documents/mio/notas cursos/tripleten/proyecto/Proyecto_sprint_6/vehicles_us.csv")
+
+
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
