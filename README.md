52b283f5c1ed7d2e6e506d640e838daebf73510f
Branlut <brayantrigoaraya@gmail.com>
2024-07-02 16:19:27 -0400

    commit para subir a render v1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Proyecto_sprint_6
+Proyecto_sprint_6
+
+El proyecto tiene la finalidad de hacer una exploración inicial de datos de anuncio de ventas de autos 
+
+los elementos en la aplicación tienen la finalidad de crear gráficos para analizar la información 
+
+los botones crearan un gráfico de histograma, dipersión y barras al ser presionados
+
+-El gráfico con el histograma es una represantacion de precio de los vehiculos
+-El gráfico de dispersión muestra la relación entre el precio y el odomotro es decir el uso o kilometraje del vehiculo
+-El gráfico de barras muestra el tipo de vehiculo y su condición
+
+Las casillas de verificación tienen la una funcion similar al de los botones pero estos mostrar el gráfico mintras este seleccionado
+
+-El gráfico de barras en este caso son 2 que muestran la cantidad de posteo por año 2018 y 2019 
+-El gráfico circular muestra  el precio por tipo de vehiculo
+
+enlace del proyecto en render
+https://proyecto-sprint-6.onrender.com
--- app.py
+++ app.py
@@ -4,18 +4,79 @@ import streamlit as st
 
 
 #car_data = pd.read_csv("C:/Users/braya/Documents/mio/notas cursos/tripleten/proyecto/Proyecto_sprint_6/vehicles_us.csv")
+car_data = pd.read_csv('vehicles_us.csv') 
+car_data['date_posted'] = pd.to_datetime(car_data['date_posted'],format="%Y-%m-%d")
+car_data['model_year'] = car_data['model_year'].fillna(1000)
+car_data['model_year'] = car_data['model_year'].astype(int)
+car_data['odometer'] = car_data['odometer'].fillna(111111).astype(int)
+car_data['cylinders'] = car_data['cylinders'].fillna(0).astype(int)
+car_data['is_4wd'] = car_data['is_4wd'].fillna(0).astype(int)
+car_data['post_year'] = car_data['date_posted'].dt.year
+car_data['post_month'] = car_data['date_posted'].dt.month
+
+#filtros
+post_2018 = car_data[car_data['post_year']==2018]['post_month'].value_counts().reset_index(name='month_count').sort_values(by='post_month')
+post_2019 = car_data[car_data['post_year']==2019]['post_month'].value_counts().reset_index(name='month_count').sort_values(by='post_month')
 
 
+st.set_page_config(layout='wide')
+st.title('Exploración de datos sobre anuncios de coches', )
+col1,col2 = st.columns(2)
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+with col1 :
+    
+    hist_button = st.button('Construir histograma')
         
-if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-    # crear un histograma
-    fig = px.histogram(car_data, x="price")
+    if hist_button: 
+        st.write('Creación de un histograma del precio de los vehiculos') 
+        fig = px.histogram(car_data, x="price")
+        st.plotly_chart(fig, use_container_width=True)
+    
+    dispersion_button = st.button('Construir Gráfico de dispersión')
+
+    if dispersion_button:
+        st.write('Gráfico de dispersión precio en relación con el odómetro(kilometraje)')
+        #fig = px.scatter(car_data, x='model_year', y='price' )
+        fig = px.scatter(car_data, x='odometer',y='price',color='type')
+        st.plotly_chart(fig,use_container_width=True)
+    
+    barras_button = st.button('Construir grafico de barras')   
+    if barras_button:
+        st.write('Gráfico de barras del tipo de vehiculo y su condición')
+        filter2 = car_data.groupby(['type','condition'])['condition'].count().reset_index(name='condition_count')
+        fig = px.bar(filter2, x='condition_count', y='type', color='condition')
+        st.plotly_chart(fig, use_container_width=True)
         
-    # mostrar un gráfico Plotly interactivo
-    st.plotly_chart(fig, use_container_width=True)
+        
+        
+with col2:    
+         
+    build_histogram = st.checkbox('crear grafico de barras')
+
+    if build_histogram: # al hacer clic en el botón
+        st.write('Vehiculos posteados el 2018 ')
+        fig = px.bar(post_2018, x="post_month",y='month_count',labels={"post_month":"meses",
+                                                                       "month_count":"cantidad"})
+        st.plotly_chart(fig, use_container_width=True)
+        
+       
+        st.write('Vehiculos posteados el 2019')
+        fig2 = px.bar(post_2019, x="post_month",y='month_count',labels={"post_month":"meses",
+                                                                       "month_count":"cantidad"},color_discrete_sequence=['#FF5733']) 
+        fig2.update_layout(
+            xaxis=dict(
+                tickmode='array',
+                tickvals=[1,2,3,4],
+                ticktext=[1,2,3,4]
+            )
+        )
+        st.plotly_chart(fig2, use_container_width=True)
+
+
+    build_bar = st.checkbox('crear barras') 
+    if build_bar:
+        st.write('Gráfico circular precio por tipo de vehiculo')
+       
+        fig = px.pie(car_data, values='price', names='type')
+        st.plotly_chart(fig, use_container_width=True)
+    
